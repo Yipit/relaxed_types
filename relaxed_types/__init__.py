e21ae105cfcd1ab97e89f4a008e234af9968b2fa
Hugo Lopes Tavares <hltbra@gmail.com>
2016-06-22 18:30:56 -0400

    Initial version

--- relaxed_types/__init__.py
+++ relaxed_types/__init__.py
@@ -1,2 +1,80 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+import functools
+import inspect
+
+
+Any = object()
+
+
+def check_type(value, expected_type, outer_value, extra=None):
+    if expected_type is Any:
+        return
+    elif isinstance(expected_type, list):
+        if not isinstance(value, list):
+            fail(value, expected_type, outer_value)
+
+        for t in expected_type:
+            for v in value:
+                check_type(v, t, outer_value)
+
+    elif isinstance(expected_type, dict):
+        if not isinstance(value, dict):
+            fail(value, expected_type, outer_value)
+
+        for key_name, key_type in expected_type.items():
+            if key_name not in value:
+                fail(value, expected_type, outer_value)
+            check_type(value[key_name], key_type, outer_value)
+
+    elif isinstance(expected_type, tuple):
+        if not isinstance(value, tuple):
+            fail(value, expected_type, outer_value)
+
+        if len(value) != len(expected_type):
+            fail(value, expected_type, outer_value)
+
+        for v, t in zip(value, expected_type):
+            check_type(v, t, outer_value)
+
+    elif inspect.isclass(expected_type):
+        if not isinstance(value, expected_type):
+            fail(value, expected_type, outer_value)
+    else:
+        if not expected_type(value):
+            fail(value, expected_type, outer_value)
+    if extra:
+        if not extra(value):
+            fail(value, expected_type, outer_value)
+
+
+def typed_return(expected_type, extra=None):
+    def wrapper(fn):
+        @functools.wraps(fn)
+        def newfn(*args, **kw):
+            result = fn(*args, **kw)
+            check_type(result, expected_type, outer_value=result, extra=extra)
+            return result
+        return newfn
+    return wrapper
+
+
+def fail(value, expected_type, outer_value):
+    raise ReturnTypeError("Type mismatch for {}, expected {}. Outer value: {}".format(
+        short_repr(value), expected_type, short_repr(outer_value)), value)
+
+
+def short_repr(obj):
+    s = repr(obj)
+    limit = 1000
+    if len(s) > limit:
+        return s[:limit] + '... [CONTENT TOO LONG]'
+    else:
+        return s[:limit]
+
+
+class ReturnTypeError(TypeError):
+    def __init__(self, msg, value):
+        super(ReturnTypeError, self).__init__(msg)
+        self.value = value
---
+++ tests/unit/test_relaxed_types.py
@@ -0,0 +1,231 @@
+import pytest
+
+from relaxed_types import typed_return, ReturnTypeError, Any
+
+
+def test_typed_return_with_simple_types():
+    decorator = typed_return(int)
+
+    @decorator
+    def int_valid():
+        return 1
+
+    @decorator
+    def int_invalid():
+        return '1'
+
+    assert int_valid() == 1
+    pytest.raises(ReturnTypeError, int_invalid)
+
+
+def test_typed_return_with_typing_types():
+    decorator = typed_return(Any)
+
+    @decorator
+    def int_valid():
+        return 1
+
+    @decorator
+    def str_valid():
+        return 'string'
+
+    assert int_valid() == 1
+    assert str_valid() == 'string'
+
+
+def test_typed_return_with_compound_types():
+    decorator = typed_return([int])
+
+    @decorator
+    def list_of_ints_valid():
+        return [1, 2, 3]
+
+    @decorator
+    def list_of_ints_invalid1():
+        return (1, 2, 3)
+
+    @decorator
+    def list_of_ints_invalid2():
+        return [1, 2, "3"]
+
+    assert list_of_ints_valid() == [1, 2, 3]
+    pytest.raises(ReturnTypeError, list_of_ints_invalid1)
+    pytest.raises(ReturnTypeError, list_of_ints_invalid2)
+
+
+def test_typed_return_with_compound_of_compound_types():
+    decorator = typed_return([[int]])
+
+    @decorator
+    def list_of_list_of_ints_valid():
+        return [[1], [2], [3]]
+
+    @decorator
+    def list_of_list_of_ints_invalid1():
+        return ([1], [2], [3])
+
+    @decorator
+    def list_of_list_of_ints_invalid2():
+        return [[1], [2], ["3"]]
+
+    assert list_of_list_of_ints_valid() == [[1], [2], [3]]
+    pytest.raises(ReturnTypeError, list_of_list_of_ints_invalid1)
+    pytest.raises(ReturnTypeError, list_of_list_of_ints_invalid2)
+
+
+def test_typed_return_with_combination():
+    decorator = typed_return(int, lambda x: x > 0)
+
+    @decorator
+    def valid1():
+        return 10
+
+    @decorator
+    def invalid1():
+        return -1
+
+    assert valid1() == 10
+    pytest.raises(ReturnTypeError, invalid1)
+
+
+def test_typed_return_with_dict_and_extra():
+    decorator = typed_return({"a": lambda x: x > 0, "b": [str]})
+
+    @decorator
+    def valid1():
+        return {"a": 1, "b": ['s']}
+
+    @decorator
+    def invalid1():
+        return {"a": ['s'], "b": 1}
+
+    @decorator
+    def invalid2():
+        return {}
+
+    @decorator
+    def invalid3():
+        return {"a": 0, "b": ['s']}
+
+    assert valid1() == {"a": 1, "b": ['s']}
+    pytest.raises(ReturnTypeError, invalid1)
+    pytest.raises(ReturnTypeError, invalid2)
+    pytest.raises(ReturnTypeError, invalid3)
+
+
+def test_typed_return_with_covariant_list():
+    decorator = typed_return([Any])
+
+    @decorator
+    def valid1():
+        return [[1], [2], [3]]
+
+    @decorator
+    def valid2():
+        return ['a', 'b', 'c']
+
+    @decorator
+    def valid3():
+        return [1, 2, 3]
+
+    assert valid1() == [[1], [2], [3]]
+    assert valid2() == ['a', 'b', 'c']
+    assert valid3() == [1, 2, 3]
+
+
+def test_typed_return_with_covariant_tuple():
+    decorator = typed_return((Any,))
+
+    @decorator
+    def valid1():
+        return ([1],)
+
+    @decorator
+    def valid2():
+        return ('a',)
+
+    @decorator
+    def valid3():
+        return (1,)
+
+    @decorator
+    def invalid1():
+        return tuple()
+
+    @decorator
+    def invalid2():
+        return [1]
+
+    assert valid1() == ([1],)
+    assert valid2() == ('a',)
+    assert valid3() == (1,)
+
+    pytest.raises(ReturnTypeError, invalid1)
+    pytest.raises(ReturnTypeError, invalid2)
+
+
+def test_typed_return_with_single_element_tuple():
+    decorator = typed_return((int,))
+
+    @decorator
+    def valid1():
+        return (1,)
+
+    @decorator
+    def invalid1():
+        return (1, 2)
+
+    @decorator
+    def invalid2():
+        return ('a',)
+
+    valid1()
+    pytest.raises(ReturnTypeError, invalid1)
+    pytest.raises(ReturnTypeError, invalid2)
+
+
+def test_typed_return_with_covariant_dict():
+    decorator = typed_return({})
+
+    @decorator
+    def valid1():
+        return {"a": 1}
+
+    @decorator
+    def valid2():
+        return {"a": 's'}
+
+    @decorator
+    def valid3():
+        return {}
+
+    valid1()
+    valid2()
+    valid3()
+
+
+def test_typed_return_with_complex_list_type():
+    decorator = typed_return(
+        [(str, int)]
+    )
+
+    @decorator
+    def valid1():
+        return [('a', 1), ('b', 2)]
+
+    @decorator
+    def valid2():
+        return []
+
+    @decorator
+    def invalid1():
+        return [1, 2, 3]
+
+    @decorator
+    def invalid2():
+        return ['a', 1, 'b']
+
+    valid1()
+    valid2()
+    pytest.raises(ReturnTypeError, invalid1)
+    pytest.raises(ReturnTypeError, invalid2)
